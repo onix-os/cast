777adb13166d371beca520484d9a0e852feb83c0
Trim Bresilla <trim.bresilla@gmail.com>
2026-07-13 01:04:11 +0200

    dev: add Nix development shell

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "OS Tools development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=4c1018dae018162ec878d42fec712642d214fdfa";
+    rust-overlay.url = "github:oxalica/rust-overlay?rev=3c27f4c92a7d977556dd2c10bb564d9c61b375e9";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rustfmt" "clippy" ];
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            rustToolchain
+            pkgs.clang
+            pkgs.cmake
+            (pkgs.diesel-cli.override {
+              sqliteSupport = true;
+              postgresqlSupport = false;
+              mysqlSupport = false;
+            })
+            pkgs.git
+            pkgs.gnumake
+            pkgs.pkg-config
+            pkgs.typos
+            pkgs.valgrind
+          ];
+
+          CC = "${pkgs.clang}/bin/clang";
+          CXX = "${pkgs.clang}/bin/clang++";
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      }
+    );
+}
